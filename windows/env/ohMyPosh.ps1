c15f7efd364ef0d142f221380a831b03b72f8102
PUPUHERO <victorwen17@gmail.com>
2024-03-09 23:25:47 +0800

    Add oh-my-posh to PowerShell profile

--- windows/env/ohMyPosh.ps1
+++ windows/env/ohMyPosh.ps1
@@ -1,11 +1,13 @@
 winget install JanDeDobbeleer.OhMyPosh -s winget
+$env:Path += ";C:\Users\user\AppData\Local\Programs\oh-my-posh\bin"
 
 if (-not (Test-Path -Path $PROFILE)) {
     New-Item -ItemType File -Path $PROFILE -Force
 }
 
-Add-Content $PROFILE 'oh-my-posh init pwsh | Invoke-Expression'
 Add-Content $PROFILE 'oh-my-posh init pwsh --config "$env:POSH_THEMES_PATH\takuya.omp.json" | Invoke-Expression'
+
+. $PROFILE
 
 sudo oh-my-posh font install Meslo
 
