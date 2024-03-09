81cb47e81c238c0d70d2361007fa0a614785c35a
PUPUHERO <victorwen17@gmail.com>
2024-03-09 23:20:31 +0800

    Update ohMyPosh initialization in PowerShell profile

--- windows/env/ohMyPosh.ps1
+++ windows/env/ohMyPosh.ps1
@@ -5,7 +5,7 @@ if (-not (Test-Path -Path $PROFILE)) {
     New-Item -ItemType File -Path $PROFILE -Force
 }
 
-Add-Content $PROFILE @'oh-my-posh init pwsh | Invoke-Expression'@
-Add-Content $PROFILE @'oh-my-posh init pwsh --config "$env:POSH_THEMES_PATH\takuya.omp.json" | Invoke-Expression'@
+Add-Content $PROFILE 'oh-my-posh init pwsh | Invoke-Expression'
+Add-Content $PROFILE 'oh-my-posh init pwsh --config "$env:POSH_THEMES_PATH\takuya.omp.json" | Invoke-Expression'
 
 Install-Module -Name Terminal-Icons -Repository PSGallery
