b6d196ff85f6c2dfaea1db703dfa84f7e7f0e7cf
PUPUHERO <victorwen17@gmail.com>
2024-03-10 04:10:50 +0800

    Commented out progressPreference in winget.ps1

--- run.ps1
+++ run.ps1
@@ -1,10 +1,14 @@
+# Install winget, pwsh
 windows\env\winget.ps1
 windows\env\pwsh.ps1
-windows\env\gsudo.ps1
-windows\env\windowsTerminal.ps1
-windows\env\chocolatey.ps1
-windows\env\ohMyPosh.ps1
 
-windows\app\vscode.ps1
+# Now can use winget and pwsh to install apps and configure the environment
+pwsh -File "windows\env\gsudo.ps1"
+pwsh -File "windows\env\windowsTerminal.ps1"
+pwsh -File "windows\env\chocolatey.ps1"
+pwsh -File "windows\env\ohMyPosh.ps1"
+
+# Install apps
+pwsh -File "windows\app\vscode.ps1"
 
 pause
