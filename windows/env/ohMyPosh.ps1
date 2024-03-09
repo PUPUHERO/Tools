70a75285d182111aeef36f511c59ae2dfa74b8b8
PUPUHERO <victorwen17@gmail.com>
2024-03-09 23:18:19 +0800

    Update ohMyPosh installation and profile creation

--- windows/env/ohMyPosh.ps1
+++ windows/env/ohMyPosh.ps1
@@ -1,9 +1,9 @@
 winget install JanDeDobbeleer.OhMyPosh -s winget
 sudo oh-my-posh font install Meslo
 
-IF NOT EXIST $PROFILE (
+if (-not (Test-Path -Path $PROFILE)) {
     New-Item -ItemType File -Path $PROFILE -Force
-)
+}
 
 Add-Content $PROFILE @'oh-my-posh init pwsh | Invoke-Expression'@
 Add-Content $PROFILE @'oh-my-posh init pwsh --config "$env:POSH_THEMES_PATH\takuya.omp.json" | Invoke-Expression'@
