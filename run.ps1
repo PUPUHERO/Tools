c405b1c099128fb1ae3e6e370cc1ef400572136c
PUPUHERO <victorwen17@gmail.com>
2024-03-11 02:14:24 +0800

    Add installation scripts for gsudo, WindowsTerminal, Chocolatey, oh-my-posh, Terminal-Icons, and VS Code

--- run.ps1
+++ run.ps1
@@ -3,19 +3,30 @@ windows\env\winget.ps1
 windows\env\pwsh.ps1
 
 # Now can use winget and pwsh to install apps and configure the environment
+Write-Host "==========Installing gsudo...=========="
 pwsh -File "windows\env\gsudo.ps1"
+
+Write-Host "==========Installing WindowsTerminal...=========="
 pwsh -File "windows\env\windowsTerminal.ps1"
+
+Write-Host "==========Installing Chocolatey...=========="
 pwsh -File "windows\env\chocolatey.ps1"
 
 # Set the environment variables
+Write-Host "==========Setting environment variables for Chocolatey and refreshenv...=========="
 $env:Path = [System.Environment]::GetEnvironmentVariable("Path","Machine") + ";" + [System.Environment]::GetEnvironmentVariable("Path","User")
 $env:ChocolateyInstall = [System.Environment]::GetEnvironmentVariable("ChocolateyInstall","Machine")
 Import-Module $env:ChocolateyInstall\helpers\chocolateyProfile.psm1
 
+# Install oh-my-posh
+Write-Host "==========Installing oh-my-posh...=========="
 pwsh -File "windows\env\ohMyPosh.ps1"
+
+Write-Host "==========Installing Terminal-Icons...=========="
 pwsh -File "windows\env\TerminalIcon.ps1"
 
 # Install apps
+Write-Host "==========Installing apps...=========="
 pwsh -File "windows\app\vscode.ps1"
 
 pause
