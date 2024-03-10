5ec4db6d04486bd8e89ae609f7d91dcdf1412dc6
PUPUHERO <victorwen17@gmail.com>
2024-03-11 02:30:58 +0800

    Update depth parameter in ConvertFrom-Json

--- windows/env/ModifyWindowsTerminalJSON.ps1
+++ windows/env/ModifyWindowsTerminalJSON.ps1
@@ -1,5 +1,5 @@
 # 讀取設定文件的內容
-$json = Get-Content -Path $env:LOCALAPPDATA\Packages\Microsoft.WindowsTerminal_8wekyb3d8bbwe\LocalState\settings.json | ConvertFrom-Json
+$json = Get-Content -Path $env:LOCALAPPDATA\Packages\Microsoft.WindowsTerminal_8wekyb3d8bbwe\LocalState\settings.json | ConvertFrom-Json -Depth 10
 
 # 修改 default profile 的 font face
 $json.profiles.default.font.face = "MesloLGM Nerd Font"
