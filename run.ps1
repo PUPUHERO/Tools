fded4dc815c4a826b49213c698565a3ad0767532
PUPUHERO <victorwen17@gmail.com>
2024-03-11 02:10:06 +0800

    Update environment variables and remove unnecessary code

--- run.ps1
+++ run.ps1
@@ -6,6 +6,12 @@ windows\env\pwsh.ps1
 pwsh -File "windows\env\gsudo.ps1"
 pwsh -File "windows\env\windowsTerminal.ps1"
 pwsh -File "windows\env\chocolatey.ps1"
+
+# Set the environment variables
+$env:Path = [System.Environment]::GetEnvironmentVariable("Path","Machine") + ";" + [System.Environment]::GetEnvironmentVariable("Path","User")
+$env:ChocolateyInstall = [System.Environment]::GetEnvironmentVariable("ChocolateyInstall","Machine")
+Import-Module $env:ChocolateyInstall\helpers\chocolateyProfile.psm1
+
 pwsh -File "windows\env\ohMyPosh.ps1"
 pwsh -File "windows\env\TerminalIcon.ps1"
 
--- windows/env/chocolatey.ps1
+++ windows/env/chocolatey.ps1
@@ -1,6 +1,1 @@
 Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
-
-# Set the environment variables
-$env:Path = [System.Environment]::GetEnvironmentVariable("Path","Machine") + ";" + [System.Environment]::GetEnvironmentVariable("Path","User")
-$env:ChocolateyInstall = [System.Environment]::GetEnvironmentVariable("ChocolateyInstall","Machine")
-Import-Module $env:ChocolateyInstall\helpers\chocolateyProfile.psm1
