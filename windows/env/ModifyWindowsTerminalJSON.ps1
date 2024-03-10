392efcf934fcff3406d774d074ccfa8f438bd4b8
PUPUHERO <victorwen17@gmail.com>
2024-03-11 02:42:14 +0800

    Update font face in Windows Terminal settings

--- windows/env/ModifyWindowsTerminalJSON.ps1
+++ windows/env/ModifyWindowsTerminalJSON.ps1
@@ -1,8 +1,13 @@
 # 讀取設定文件的內容
 $json = Get-Content -Path $env:LOCALAPPDATA\Packages\Microsoft.WindowsTerminal_8wekyb3d8bbwe\LocalState\settings.json | ConvertFrom-Json -Depth 10
 
-# 修改 default profile 的 font face
-$json.profiles.default.font.face = "MesloLGM Nerd Font"
+# 創建一個新的對象
+$font = New-Object PSObject -Property @{
+    face = "MesloLGM Nerd Font"
+}
+
+# 將新的對象賦值給 $json.profiles.default.font
+$json.profiles.default.font = $font
 
 # 將新的內容寫入設定文件
 $json | ConvertTo-Json | Set-Content -Path $env:LOCALAPPDATA\Packages\Microsoft.WindowsTerminal_8wekyb3d8bbwe\LocalState\settings.json
