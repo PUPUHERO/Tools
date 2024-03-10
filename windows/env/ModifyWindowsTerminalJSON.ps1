1e31577df9fb6b2ad3da49b0a6ca937f6bb27b3c
PUPUHERO <victorwen17@gmail.com>
2024-03-11 02:48:19 +0800

    Add font configuration to Windows Terminal JSON

--- windows/env/ModifyWindowsTerminalJSON.ps1
+++ windows/env/ModifyWindowsTerminalJSON.ps1
@@ -1,13 +1,12 @@
 # 讀取設定文件的內容
 $json = Get-Content -Path $env:LOCALAPPDATA\Packages\Microsoft.WindowsTerminal_8wekyb3d8bbwe\LocalState\settings.json | ConvertFrom-Json -Depth 10
 
-# 創建一個新的對象
-$font = New-Object PSObject -Property @{
-    face = "MesloLGM Nerd Font"
-}
-
 # 將新的對象賦值給 $json.profiles.default
-$json.profiles.default | Add-Member -Type NoteProperty -Name font -Value $font
+if (!$json.profiles.default.fonts) {
+    $json.profiles | Add-Member -MemberType NoteProperty -Name "fonts" -Value ([PSCustomObject]@{ 
+    face = "MesloLGM Nerd Font"
+    })
+}
 
 # 將新的內容寫入設定文件
 $json | ConvertTo-Json -Depth 10 | Set-Content -Path $env:LOCALAPPDATA\Packages\Microsoft.WindowsTerminal_8wekyb3d8bbwe\LocalState\settings.json
