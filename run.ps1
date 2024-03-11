61383639a1e6c5ff68ea78e5b597eb555187e81b
PUPUHERO <victorwen17@gmail.com>
2024-03-11 20:32:08 +0800

    Remove unnecessary code and modify Windows Terminal initialization and settings

--- run.ps1
+++ run.ps1
@@ -9,13 +9,6 @@ windows\env\gsudo.ps1
 Write-Host "==========Installing WindowsTerminal...=========="
 windows\env\windowsTerminal.ps1
 
-# Open and close Windows Terminal for the first time opening initialization
-$terminal = Start-Process wt -PassThru
-Start-Sleep -Seconds 2
-if (Get-Process -Id $terminal.Id -ErrorAction SilentlyContinue) {
-    Stop-Process -Id $terminal.Id
-}
-
 Write-Host "==========Installing Chocolatey...=========="
 windows\env\chocolatey.ps1
 
@@ -32,10 +25,6 @@ refreshenv
 
 Write-Host "==========Installing Terminal-Icons...=========="
 pwsh -File "windows\env\TerminalIcon.ps1"
-
-# Modify the settings.json of Windows Terminal
-Write-Host "==========Modifying the settings.json of Windows Terminal...=========="
-pwsh -File "windows\env\ModifyWindowsTerminalJSON.ps1"
 
 # Install apps
 Write-Host "==========Installing apps...=========="
--- windows/env/windowsTerminal.ps1
+++ windows/env/windowsTerminal.ps1
@@ -1,1 +1,12 @@
 winget install -e --id Microsoft.WindowsTerminal
+
+# Open and close Windows Terminal for the first time opening initialization
+$terminal = Start-Process wt -PassThru
+Start-Sleep -Seconds 2
+if (Get-Process -Id $terminal.Id -ErrorAction SilentlyContinue) {
+    Stop-Process -Id $terminal.Id
+}
+
+# Modify the settings.json of Windows Terminal
+Write-Host "==========Modifying the settings.json of Windows Terminal...=========="
+windows\env\ModifyWindowsTerminalJSON.ps1
