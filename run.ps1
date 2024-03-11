bba72534acd5ffbe4cb40a9b5dd140817d54c8da
PUPUHERO <victorwen17@gmail.com>
2024-03-11 21:09:04 +0800

    Update installation scripts for scoop and Chocolatey

--- run.ps1
+++ run.ps1
@@ -1,14 +1,11 @@
 # Install winget, pwsh
 windows\env\winget.ps1
-windows\env\pwsh.ps1
 
-# Now can use winget and pwsh to install apps and configure the environment
-Write-Host "==========Installing gsudo...=========="
-windows\env\gsudo.ps1
+# Install scoop
+Write-Host "==========Installing scoop...=========="
+windows\env\scoop.ps1
 
-Write-Host "==========Installing WindowsTerminal...=========="
-windows\env\windowsTerminal.ps1
-
+# Install Chocolatey
 Write-Host "==========Installing Chocolatey...=========="
 windows\env\chocolatey.ps1
 
@@ -17,6 +14,20 @@ Write-Host "==========Setting environment variables for Chocolatey and refreshen
 $env:Path = [System.Environment]::GetEnvironmentVariable("Path","Machine") + ";" + [System.Environment]::GetEnvironmentVariable("Path","User")
 $env:ChocolateyInstall = [System.Environment]::GetEnvironmentVariable("ChocolateyInstall","Machine")
 Import-Module $env:ChocolateyInstall\helpers\chocolateyProfile.psm1
+
+# The followings are install by winget
+# Install pwsh
+Write-Host "==========Installing pwsh...=========="
+windows\env\pwsh.ps1
+refreshenv
+
+# Now can use winget and pwsh to install apps and configure the environment
+Write-Host "==========Installing gsudo...=========="
+windows\env\gsudo.ps1
+refreshenv
+
+Write-Host "==========Installing WindowsTerminal...=========="
+windows\env\windowsTerminal.ps1
 
 # Install oh-my-posh
 Write-Host "==========Installing oh-my-posh...=========="
